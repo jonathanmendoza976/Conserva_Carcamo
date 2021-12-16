9acd0b317a4f3a331267c8f7a710eac52b52ebff
DerianCordobaPerez <derianunanleon@gmail.com>
2021-12-15 21:03:39 -0600

    Save images in server with Multer

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,4 +1,5 @@
 import Express from 'express'
+import {uploadImages} from '../middlewares/multer'
 import {index, show, create, store, edit, update, remove} from '../controllers/product.controller'
 
 const router = Express.Router()
@@ -6,7 +7,7 @@ const router = Express.Router()
 router.get('/products', index)
 router.get('/products/:id/show', show)
 router.get('/products/create', create)
-router.post('/products/create', store)
+router.post('/products/create', uploadImages('images'), store)
 router.get('/products/:id/edit', edit)
 router.post('/products/:id/edit', update)
 router.post('/products/:id/delete', remove)
